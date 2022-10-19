4596467b5ad5099e269d7a25f272b567da1c5bff
monicagellar1234 <116184442+monicagellar1234@users.noreply.github.com>
2022-10-19 20:16:05 +0530

    Add files via upload

---
+++ 1add.cpp
@@ -0,0 +1,13 @@
+/*Addition of two numbers*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter a number: ";
+    cin>>b;
+    c=a+b;
+    cout<<"The addition is "<<c;
+}
